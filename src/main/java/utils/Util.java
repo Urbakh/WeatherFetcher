ba11f6532680e030fe96f1c511e49b8c8aef0ae2
Urbakh <94566003+Urbakh@users.noreply.github.com>
2023-11-01 16:52:17 +0300

    added util class, fix bug connection

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -7,8 +7,10 @@ public class Util {
 
     public static Connection JDBCConnection() {
         try {
-            Class.forName("org.postgresql.Driver");
-            return DriverManager.getConnection("db.url", "db.username", "db.password");
+            Class.forName(PropertiesUtil.get("db.driver"));
+            return DriverManager.getConnection(PropertiesUtil.get("db.url"),
+                    PropertiesUtil.get("db.username"),
+                    PropertiesUtil.get("db.password"));
         } catch (Exception e) {
             throw new RuntimeException();
         }
