7fb312b7abaef876baab7644f40bf991c64ea7b8
Urbakh <94566003+Urbakh@users.noreply.github.com>
2023-11-01 16:59:10 +0300

    Update Util.java

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -3,14 +3,22 @@ package utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class Util {
+public final class Util {
+
+    private static final String URL_KEY = "db.url";
+
+    private static final String USERNAME_KEY = "db.username";
+    
+    private static final String PASSWORD_KEY = "db.password";
+
+    private static final String DRIVER_KEY = "db.driver";
 
     public static Connection JDBCConnection() {
         try {
-            Class.forName(PropertiesUtil.get("db.driver"));
-            return DriverManager.getConnection(PropertiesUtil.get("db.url"),
-                    PropertiesUtil.get("db.username"),
-                    PropertiesUtil.get("db.password"));
+            Class.forName(PropertiesUtil.get(DRIVER_KEY));
+            return DriverManager.getConnection(PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY));
         } catch (Exception e) {
             throw new RuntimeException();
         }
